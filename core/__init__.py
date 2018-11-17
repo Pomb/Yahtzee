401399051802363fc094e9e04975a28964530a13
Paul <pmlomb@gmail.com>
2018-11-17 13:01:11 +0200

    removed completed todos and silenced __init__ warnings with #noqa

--- core/__init__.py
+++ core/__init__.py
@@ -1,8 +1,8 @@
-from .jsonSave import JsonSave
-from .dice import d6Set
-from .command import *
-from .player import Player
-from .turn import Turn
-from .titleVisualizer import TitleVisualizer
-from .scoreRules import ScoreRules
-from .gameData import GameData
+from .jsonSave import JsonSave  # noqa
+from .dice import d6Set  # noqa
+from .command import *  # noqa
+from .player import Player  # noqa
+from .turn import Turn  # noqa
+from .titleVisualizer import TitleVisualizer  # noqa
+from .scoreRules import ScoreRules  # noqa
+from .gameData import GameData  # noqa
